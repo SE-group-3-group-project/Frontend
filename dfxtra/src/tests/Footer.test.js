1774a36c0fe8d40cb23d14218a77398df6f867ff
QianZhang19 <qianzhang019@gmail.com>
2022-12-06 14:36:17 +0000

    bug solved & Footer test pass

--- dfxtra/src/tests/Footer.test.js
+++ dfxtra/src/tests/Footer.test.js
@@ -1,8 +1,9 @@
-const render = require('@testing-library/react');
-const Footer = require('../components/Footer');
+import { render } from '@testing-library/react';
+import Footer from '../components/Footer';
 
 test(`Footer matches UI snapshot`, () => {
     const footerComponent = render(<Footer />);
 
     expect(footerComponent).toMatchSnapshot();
-})
+});
+
