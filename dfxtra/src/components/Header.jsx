cd05cd3995c7185df0d88314850aebabbb7374a1
Justin Ho <hochungyan@live.hk>
2022-12-07 12:08:27 +0000

    Need help text added to the Header

--- dfxtra/src/components/Header.jsx
+++ dfxtra/src/components/Header.jsx
@@ -2,11 +2,17 @@ import logo from './images/logo.svg';
 
 const Header = () => {
   return (
-    <div className="bg-black-blue">
+    <div className="text-white bg-black-blue">
     <nav className="navbar navbar-expand-sm">
  <a href="https://www.digitalfutures.com" className="navbar-brand" target="_blank" rel="noreferrer">
    <img src={logo} alt="Digital Futures" width="300" />
  </a>
+ <div class="col-2 text-right">
+  <a href="/" className="navbar-brand">
+         Need help? Talk to us Menu
+        </a>
+</div>
+
 </nav>
  </div>
 
