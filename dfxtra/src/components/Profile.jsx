fc6ffd00a51b84f11449d9326407a86d2c52fe26
Kaycee Gu <kayceegu@Kaycees-MacBook-Air.local>
2022-12-12 15:40:12 +0000

    completed frontend GET request excluding school subjects

--- dfxtra/src/components/Profile.jsx
+++ dfxtra/src/components/Profile.jsx
@@ -1,134 +1,75 @@
 // import StorySummary from "./StorySummary";
 import { Link } from "react-router-dom"
+import StorySummary from "./StorySummary";
 
-const Profile = ({ name, pEmail, dfEmail, github, linkedin, phone }) => {
+const Profile = ({ name, profilePicture, pEmail, dfEmail, github, linkedin, phone, story }) => {
     return (
-        <div className="bg-light-grey py-6 px-4">
-            <div className="px-12 py-6 my-4 bg-off-white rounded-2xl w-full mx-auto">
+        <div className="bg-light-grey py-6 px-4 sm:px-12">
+            <div className="px-12 py-6 my-y bg-off-white rounded-2xl">
                 <div className="flex flex-row items-center gap-6">
-                    <h1 className="text-3xl py-2">Your Profile</h1>
+                    <h1 className="text-3xl py-2 place-content-left">Your Profile</h1>
                     <Link to="/edit">
                         <button className="bg-light-grey text-royal-blue border rounded px-3 py-0.5" id="edit-button">Edit</button>
                     </Link>
                 </div>
+                <div className="w-full flex flex-col items-center">
 
-                <div className="flex flex-col lg:grid lg:grid-cols-2 lg:gap-2 pt-8 pb-6 px-2 sm:px-6">
-                    <div className="w-full max-w-md">
-                        <div className="flex flex-col md:flex-row items-start sm:items-center justify-between pb-4">
-                            <p className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="first-name">
-                                Name
-                            </p>
-                            <p className="appearance-none block w-fit sm:w-3/5 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white">
-                                {name}
-                            </p>
+                    <div className="flex flex-col items-center justify-between gap-8 lg:grid lg:grid-cols-2 lg:gap-40 pt-8 pb-6 pr-2 sm:px-6 max-w-6xl">
+                        <div className="flex flex-wrap justify-center">
+                            <img src={profilePicture} alt="grad profile" style={{ "width": "300px", "height": "300px", "borderRadius": "50%" }} className="max-w-md" />
                         </div>
-                        <div className="flex flex-col md:flex-row items-start sm:items-center justify-between pb-4">
-                            <p className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="first-name">
-                                Personal Email
-                            </p>
-                            <p className="appearance-none block w-fit sm:w-3/5  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white">
-                                {pEmail}
-                            </p>
-                        </div>
-                        <div className="flex flex-col md:flex-row items-start sm:items-center justify-between pb-4">
-                            <p className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="first-name">
-                                Digital Futures Email
-                            </p>
-                            <p className="appearance-none block w-fit sm:w-3/5 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white">
-                                {dfEmail}
-                            </p>
-                        </div>
-                    </div>
-                    <div className="w-full max-w-md">
-                        <div className="flex flex-col md:flex-row items-start sm:items-center justify-between pb-4">
-                            <p className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="first-name">
-                                Github Account
-                            </p>
-                            <p className="appearance-none block w-fit sm:w-3/5 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white">
-                                {github}
-                            </p>
-                        </div>
-                        <div className="flex flex-col md:flex-row items-start sm:items-center justify-between pb-4">
-                            <p className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="first-name">
-                                LinkedIn Account
-                            </p>
-                            <p className="appearance-none block w-fit sm:w-3/5  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white">
-                                {linkedin}
-                            </p>
-                        </div>
-                        <div className="flex flex-col md:flex-row items-start sm:items-center justify-between pb-4">
-                            <p className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="first-name">
-                                Phone Number
-                            </p>
-                            <p className="appearance-none block w-fit sm:w-3/5 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white">
-                                {phone}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="w-full max-w-4xl md:max-w-6xl py-2">
-                    <p className="font-semibold uppercase tracking-wide text-gray-700 text-sm font-bold mb-3">Personal Story Summary:</p>
-                    <div className="border border-light-blue rounded-lg py-1 px-4">
-                        <div className="my-2 sm:py-2 border-b border-dashed border-mid-grey border-opacity-20">
-                            <p className="font-medium">Work experiences:</p>
-                            <div className="sm:grid sm:grid-cols-4 sm:gap-y-2 sm:gap-x-4 py-2">
-                                <p>2015-2020</p>
-                                <p>Architect</p>
-                                <p>ArchitectCorp LLP</p>
+                        <div className="w-full max-w-md">
+                            <div className="flex flex-col md:flex-row items-start sm:items-center justify-between pb-4">
+                                <p className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="first-name">
+                                    Name
+                                </p>
+                                <p className="appearance-none block w-fit sm:w-3/5 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white">
+                                    {name}
+                                </p>
                             </div>
-                        </div>
-                        <div className="my-2 sm:py-2 border-b borer-dashed border-mid-grey border-opacity-20">
-                            <p className="font-medium">Degree:</p>
-                            <div className="sm:grid sm:grid-cols-4 sm:gap-y-2 sm:gap-x-4 py-2">
-                                <p>2012-2015</p>
-                                <p>School of Life</p>
-                                <p>Philosophy</p>
-                                <p>2:1</p>
+                            <div className="flex flex-col md:flex-row items-start sm:items-center justify-between pb-4">
+                                <p className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="first-name">
+                                    Personal Email
+                                </p>
+                                <p className="appearance-none block w-fit sm:w-3/5  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white">
+                                    {pEmail}
+                                </p>
                             </div>
-                        </div>
-                        <div className="my-2 sm:py-2 border-b border-dashed border-mid-grey border-opacity-20">
-                            <p className="font-medium">School:</p>
-                            <div className="sm:grid sm:grid-cols-4 sm:gap-y-2 sm:gap-x-4 py-2">
-                                <p>2006-2012</p>
-                                <p>Friday High School</p>
-                                <div className="flex flex-col">
-                                    <p>Maths</p>
-                                    <p>Physics</p>
-                                    <p>Art</p>
-                                    <p>Textiles</p>
-                                </div>
-                                <div className="flex flex-col">
-                                    <p>A*</p>
-                                    <p>A</p>
-                                    <p>B</p>
-                                    <p>C</p>
-                                </div>
+                            <div className="flex flex-col md:flex-row items-start sm:items-center justify-between pb-4">
+                                <p className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="first-name">
+                                    Digital Futures Email
+                                </p>
+                                <p className="appearance-none block w-fit sm:w-3/5 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white">
+                                    {dfEmail}
+                                </p>
                             </div>
-                        </div>
-                        <div className="my-2 sm:py-2 border-b border-dashed border-mid-grey border-opacity-20">
-                            <p className="font-medium">Certifications:</p>
-                            <div className="sm:grid sm:grid-cols-4 sm:gap-y-2 sm:gap-x-4 py-2">
-                                <p className="font-medium">Java SE 8 Programmer I</p>
-                                <p>Oracle</p>
-                                <p>Pass</p>
-                                <p>2029</p>
+                            <div className="flex flex-col md:flex-row items-start sm:items-center justify-between pb-4">
+                                <p className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="first-name">
+                                    Github Account
+                                </p>
+                                <p className="appearance-none block w-fit sm:w-3/5 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white">
+                                    {github}
+                                </p>
                             </div>
-                        </div>
-                        <div className="my-2 sm:py-2">
-                            <p className="font-medium">Personal Achievements:</p>
-                            <div className="sm:grid sm:grid-cols-2 sm:gap-y-2 sm:gap-x-4 py-2">
-                                <div className="flex flex-row gap-6">
-                                    <p className="font-medium">Juggling:</p>
-                                    <p>Regional Juggling Champion</p>
-                                </div>
-                                <div className="flex flex-row gap-6">
-                                    <p className="font-medium">Axe-throwing:</p>
-                                    <p>Some description about axe throwing</p>
-                                </div>
+                            <div className="flex flex-col md:flex-row items-start sm:items-center justify-between pb-4">
+                                <p className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="first-name">
+                                    LinkedIn Account
+                                </p>
+                                <p className="appearance-none block w-fit sm:w-3/5  bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white">
+                                    {linkedin}
+                                </p>
+                            </div>
+                            <div className="flex flex-col md:flex-row items-start sm:items-center justify-between pb-4">
+                                <p className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="first-name">
+                                    Phone Number
+                                </p>
+                                <p className="appearance-none block w-fit sm:w-3/5 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-2 leading-tight focus:outline-none focus:bg-white">
+                                    {phone}
+                                </p>
                             </div>
                         </div>
                     </div>
+                    <StorySummary story={story} />
                 </div>
             </div>
         </div >
--- dfxtra/src/components/StorySummary.jsx
+++ dfxtra/src/components/StorySummary.jsx
@@ -1,11 +1,131 @@
+import { all } from "axios";
 
-// const StorySummary = (story) => {
-//     <div className="story">
-//         <p>Personal Story Summary:</p>
-//         <div className="summary"></div>
-//         {story.degrees.map(degrees => (
-//             <p>Degree: A degree in {degrees.degreeSubject} from {degrees.university} </p>
-//         ))}
-//     </div>
-// }
-// export default StorySummary;
+const StorySummary = ({ key, story }) => {
+
+    const date = (startDate, endDate) => {
+        const d = new Date(startDate);
+        const ed = new Date(endDate);
+        let startMonth = d.toLocaleDateString('en-uk', { month: "long" });
+        let startYear = d.getFullYear();
+        let endMonth = ed.toLocaleDateString('en-uk', { month: "long" });
+        let endYear = ed.getFullYear();
+        let period = startMonth + " " + startYear + " - " + endMonth + " " + endYear
+        return period
+    }
+
+    const allWorkExperience =
+        story.workExperiences.map(work => {
+            if (story.workExperiences.length > 0) {
+                key = Math.random();
+                return (
+                    <div className="sm:grid sm:grid-cols-4 sm:gap-y-2 sm:gap-x-4 py-2" key={key}>
+                        <p>{date(work.startDate, work.endDate)}</p>
+                        <p>{work.type}</p>
+                        <p>{work.position}</p>
+                        <p>{work.employer}</p>
+                    </div>
+                )
+            }
+        })
+
+    const allDegree =
+        story.degrees.map(degree => {
+            if (story.degrees.length > 0) {
+                key = Math.random();
+                return (
+                    <div className="sm:grid sm:grid-cols-4 sm:gap-y-2 sm:gap-x-4 py-2" key={key}>
+                        <p>{date(degree.startDate, degree.endDate)}</p>
+                        <p>{degree.university}</p>
+                        <p>{degree.degreeSubject}</p>
+                        <p>{degree.grade}</p>
+                    </div>
+                )
+            }
+        })
+
+    const allSchoolQualification =
+        story.schoolQualifications.map(qualification => {
+            if (story.schoolQualifications.length > 0) {
+                key = Math.random();
+                return (
+                    <div className="sm:grid sm:grid-cols-4 sm:gap-y-2 sm:gap-x-4 py-2" key={key}>
+                        <p>{date(qualification.startDate, qualification.endDate)}</p>
+                        <p>{qualification.school}</p>
+                        <div className="flex flex-col">
+                            <p>{qualification.subject}</p>
+                            <p>Maths</p>
+                            <p>Art</p>
+                            <p>Textiles</p>
+                        </div>
+                        <div className="flex flex-col">
+                            <p>{qualification.grade}</p>
+                            <p>A</p>
+                            <p>B</p>
+                            <p>C</p>
+                        </div>
+                    </div>
+                )
+            }
+        })
+
+    const allCertification =
+        story.certifications.map(certification => {
+            if (story.certifications.length > 0) {
+                key = Math.random();
+                return (
+                    <div className="sm:grid sm:grid-cols-4 sm:gap-y-2 sm:gap-x-4 py-2" key={key}>
+                        <p className="font-medium">{certification.type}</p>
+                        <p>{certification.issuer}</p>
+                        <p>{certification.grade}</p>
+                        <p>{certification.year}</p>
+                    </div>
+                )
+            }
+        })
+
+    const allAchievement =
+        story.personalAchievements.map(achievement => {
+            if (story.personalAchievements.length > 0) {
+                key = Math.random();
+                return (
+                    <>
+                        <div className="flex flex-row gap-6">
+                            <p className="font-medium">{achievement.achievement}:</p>
+                            <p>{achievement.description}</p>
+                        </div>
+                    </>
+                )
+            }
+        })
+
+    return (
+        <div className="w-full max-w-4xl md:max-w-6xl py-2">
+            <p className="font-semibold uppercase tracking-wide text-gray-700 text-sm font-bold mb-3">Personal Story Summary:</p>
+            <div className="border border-light-blue rounded-lg py-1 px-4">
+                <div className="my-2 sm:py-2 border-b border-dashed border-mid-grey border-opacity-20">
+                    <p className="font-medium">Work experiences:</p>
+                    {allWorkExperience}
+                </div>
+                <div className="my-2 sm:py-2 border-b borer-dashed border-mid-grey border-opacity-20">
+                    <p className="font-medium">Degree:</p>
+                    {allDegree}
+                </div>
+                <div className="my-2 sm:py-2 border-b border-dashed border-mid-grey border-opacity-20">
+                    <p className="font-medium">School:</p>
+                    {allSchoolQualification}
+                </div>
+                <div className="my-2 sm:py-2 border-b border-dashed border-mid-grey border-opacity-20">
+                    <p className="font-medium">Certifications:</p>
+                    {allCertification}
+                </div>
+                <div className="my-2 sm:py-2">
+                    <p className="font-medium">Personal Achievements:</p>
+                    <div className="sm:grid sm:grid-cols-2 sm:gap-y-2 sm:gap-x-4 py-2">
+                        {allAchievement}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default StorySummary;
