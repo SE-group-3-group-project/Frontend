c54fc14aa981d150b524633ed2918e0473687ecf
Jac <jac.mckeigue@outlook.com>
2022-12-12 14:22:43 +0000

    added login component (with gremlins)

--- dfxtra/src/App.js
+++ dfxtra/src/App.js
@@ -2,10 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import 'tailwindcss/tailwind.css';
 import axios from 'axios';
-import Edit from './pages/Edit';
+// import Edit from './pages/Edit';
 import UserHome from './pages/UserHome';
-import Login from './components/login/Login';
-import Register from './components/register/Register';
+import Login from './components/Login.jsx';
+// import Register from './components/register/Register';
 
 function App() {
   const [user, setLoginUser] = useState({});
@@ -33,8 +33,18 @@ function App() {
     <>
       <Router>
         <Routes>
+          <Route
+            exact path="/"
+            element={
+              <>
+                <Login setLoginUser={setLoginUser} />
+              </>
+            } />
+          <Route path="/login" element={<Login setLoginUser={setLoginUser} />} />
+          {/* for adding later */}
+          {/* <Route path="/register" element={<Register />} /> */}
           <Route path="/" element={<UserHome data={profile} />} />
-          <Route path="/edit" element={<Edit />} />
+          {/* <Route path="/edit" element={<Edit />} /> */}
         </Routes>
       </Router>
     </>
---
+++ dfxtra/src/components/Login.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { Link, Navigate, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const Login = ({ setLoginUser }) => {
+    const [user, setUser] = useState({
+        email: ``,
+        password: ``
+    });
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    const location = useLocation();
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setUser({
+            ...user,
+            [name]: value
+        });
+    };
+
+    const login = async (e) => {
+        e.preventDefault();
+
+        const res = await axios.post(`http://localhost:4000/login`, user);
+        alert(res.data.message);
+        console.log(res);
+        setLoggedIn(res.data.user ? true : false);
+        setUser({ personalEmail: ``, password: `` });
+        setLoginUser(res.data.user);
+        console.log(res);
+    };
+
+    const logout = async (e) => {
+        e.preventDefault();
+        const res = await axios.post(`http://localhost:4000/login`, user);
+        setLoginUser(res.data.user ? false : true);
+        setLoggedIn({ LoggedIn: false })
+        console.log("You are logged out")
+        alert('You have been logged out of the system');
+    }
+
+    return (
+        <>
+            {loggedIn && <Navigate to="/" state={{ from: location }} />}
+            <h3>Log in to your account</h3>
+            <form onSubmit={login}>
+                <input type="email" id="sign-in-email" name="personal-email" value={user.personalEmail} onChange={handleChange} placeholder="Your email" />
+                <br />
+                <input type="password" id="sign-in-password" name="password" value={user.password} onChange={handleChange} placeholder="Your password" />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
+            <Link to="/register">
+                You don&#x27;t have an account? Register now!
+            </Link>
+            <form onSubmit={logout}>
+                <button id="logout" onSubmit={logout}>Log Out</button>
+            </form>
+        </>
+    );
+}
+
+Login.propTypes = {
+    setLoginUser: PropTypes.func.isRequired
+}
+
+export default Login;
