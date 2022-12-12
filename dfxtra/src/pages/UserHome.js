b4465076b02c86bc6ae89f96112582f759297fd2
AnishShah1803 <anish.shah1803@gmail.com>
2022-12-12 13:57:01 +0000

    updated UserHome to access just the single id profile again

--- dfxtra/src/pages/UserHome.js
+++ dfxtra/src/pages/UserHome.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:dfxtra/src/components/UserHome.jsx
 import Profile from "../components/Profile";
 import Footer from "../components/Footer";
 import HomeHeader from "../components/HomeHeader";
@@ -30,30 +29,6 @@ const UserHome = ({ data }) => {
 			</>
 		);
 	}
-=======
-import Profile from '../components/Profile';
-import Footer from '../components/Footer';
-import HomeHeader from '../components/HomeHeader';
-
-const UserHome = ({ data }) => {
-    return (
-        <>
-            <HomeHeader />
-            {data.map(profile => (
-                <Profile
-                    key={profile._id.toString()}
-                    name={profile.name.first + " " + profile.name.last + " (" + profile.pronoun.replaceAll(`-`, `/`) + ")"}
-                    pEmail={profile.contactDetails.personalEmail}
-                    dfEmail={profile.contactDetails.dfEmail}
-                    github={profile.contactDetails.github}
-                    linkedin={profile.contactDetails.linkedIn}
-                    phone={profile.contactDetails.phoneNumber}
-                />
-            ))}
-            <Footer />
-        </>
-    );
->>>>>>> 2b5b959e1fa9f0a2459ac0e0872a3a706535f149:dfxtra/src/pages/UserHome.js
 };
 
 export default UserHome;
