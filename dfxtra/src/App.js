81268a03ec17aa9db3aa26bb004a10a979d1e1a5
AnishShah1803 <anish.shah1803@gmail.com>
2022-12-12 13:51:13 +0000

    updated UserHome to access just the single id profile

--- dfxtra/src/App.js
+++ dfxtra/src/App.js
@@ -1,17 +1,43 @@
-import Footer from './components/Footer';
-import './App.css';
-import 'tailwindcss/tailwind.css';
-
-import mockData from './Utils/MockData.json';
-import UserHome from './components/UserHome';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import "tailwindcss/tailwind.css";
+import axios from "axios";
+import Edit from "./pages/Edit";
+import UserHome from "./pages/UserHome";
 
 function App() {
-  return (
-    <>
-      <UserHome data={mockData} />
-      <Footer />
-    </>
-  );
+	const [profile, setProfile] = useState([]);
+	const [error, setError] = useState("");
+
+	const getProfile = async () => {
+		try {
+			const response = await axios.get(
+				"http://localhost:4000/gradprofile/6395ae2116838db6d3b67cbe"
+			);
+			setProfile(response.data);
+		} catch (error) {
+			setError(error.message);
+		}
+	};
+
+	useEffect(() => {
+		getProfile();
+	}, []);
+
+	if (error) {
+		setError({ error });
+	}
+
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route path="/" element={<UserHome data={profile} />} />
+					<Route path="/edit" element={<Edit />} />
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
 export default App;
--- dfxtra/src/components/UserHome.jsx
+++ dfxtra/src/components/UserHome.jsx
@@ -1,23 +1,34 @@
-import Profile from './Profile';
+import Profile from "../components/Profile";
+import Footer from "../components/Footer";
+import HomeHeader from "../components/HomeHeader";
 
 const UserHome = ({ data }) => {
-    return (
-        <>
-
-            {data.map(profile => (
-                <Profile
-                    key={profile._id}
-                    name={profile.name.first}
-                    pEmail={profile.contactDetails.personalEmail}
-                    dfEmail={profile.contactDetails.dfEmail}
-                    github={profile.contactDetails.github}
-                    linkedin={profile.contactDetails.linkedIn}
-                    phone={profile.contactDetails.phoneNumber}
-                    story={profile.personalStories}
-                />
-            ))}
-        </>
-    );
+	if (Object.keys(data).length > 0) {
+		const key = data._id.toString();
+		console.log(key);
+		const { first, last } = data.name;
+		const pronouns = data.pronoun.replaceAll("-", "/");
+		const pEmail = data.contactDetails.personalEmail;
+		const dfEmail = data.contactDetails.dfEmail;
+		const github = data.contactDetails.github;
+		const linkedIn = data.contactDetails.linkedIn;
+		const phone = data.contactDetails.phoneNumber;
+		return (
+			<>
+				<HomeHeader />
+				<Profile
+					key={key}
+					name={first + " " + last + " (" + pronouns + ")"}
+					pEmail={pEmail}
+					dfEmail={dfEmail}
+					github={github}
+					linkedin={linkedIn}
+					phone={phone}
+				/>
+				<Footer />
+			</>
+		);
+	}
 };
 
 export default UserHome;
