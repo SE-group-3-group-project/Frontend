2c5c91bf47f11e08d33a9139a1009f2b5b77fbf7
AnishShah1803 <anish.shah1803@gmail.com>
2022-12-14 17:44:58 +0000

    Fixed file name errors

--- dfxtra/src/components/EditPageForms/SchoolForm.jsx
+++ dfxtra/src/components/EditPageForms/SchoolForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import TextInput from "../../Utils/Input.jsx"
+import TextInput from "../../utils/Input.jsx"
 
 const SchoolForm = ({ inputSchoolQualifications, setSchoolQualifications }) => {
     let schoolName;
